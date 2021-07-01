cbefe3375b06533654cb992af3977ca860f00342
KimWanJoo <wjwan0915@gmail.com>
2021-07-01 12:46:08 +0900

    no message

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -15,7 +15,6 @@ public class test extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
