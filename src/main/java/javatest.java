f1876899e887fbbd77ee9499435dd72ab701b7cf
KimWanJoo <wjwan0915@gmail.com>
2021-07-02 16:01:59 +0900

    no message

--- src/main/java/javatest.java
+++ src/main/java/javatest.java
@@ -1,11 +1,8 @@
 public class javatest {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("얄리얄리얄랴셩");
-=======
         System.out.println("테스트");
         System.out.println("테스트");
         System.out.println("테스트");
->>>>>>> develop
     }
 }
