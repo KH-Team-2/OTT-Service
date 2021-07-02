c5094d69b5b43e1c71510167b1bdc2e1de930574
Ju Dong Jin <wnehdwlswn@gmail.com>
2021-07-02 19:32:35 +0900

    [ FBW, PlatformCode ] DTO

---
+++ src/main/java/com/fbw/dto/FBWDto.java
@@ -0,0 +1,25 @@
+package com.fbw.dto;
+
+public class FBWDto {
+	
+	private String FBWords;
+	private String Reason;
+	
+	public FBWDto(String fBWords, String reason) {
+		super();
+		FBWords = fBWords;
+		Reason = reason;
+	}
+	public String getFBWords() {
+		return FBWords;
+	}
+	public void setFBWords(String fBWords) {
+		FBWords = fBWords;
+	}
+	public String getReason() {
+		return Reason;
+	}
+	public void setReason(String reason) {
+		Reason = reason;
+	}
+}
