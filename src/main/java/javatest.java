234fc2ba016d8c281336ab866bffc92f588bd2f9
KimWanJoo <wjwan0915@gmail.com>
2021-07-02 16:02:31 +0900

    테스트파일 삭제

--- src/main/java/javatest.java
+++
@@ -1,7 +0,0 @@
-public class javatest {
-    public static void main(String[] args) {
-        System.out.println("테스트");
-        System.out.println("테스트");
-        System.out.println("테스트");
-    }
-}
