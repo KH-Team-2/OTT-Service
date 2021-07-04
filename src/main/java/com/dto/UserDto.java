cb3d52fa3712aa419b4b40c8768ccd7ee6d36bb6
Dahye Jeong <moonbird92@gmail.com>
2021-07-05 00:59:13 +0900

    userdto 수정

--- src/main/java/com/dto/UserDto.java
+++ src/main/java/com/dto/UserDto.java
@@ -1,6 +1,6 @@
 package com.dto;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class UserDto {
 	private int UserNum;
@@ -16,6 +16,10 @@ public class UserDto {
 	private String Status;
 	private String Grade;
 	private Date UserDate;
+	
+	public UserDto() {
+		super();
+	}
 	
 	public UserDto(int userNum, String iD, String pW, String email, String phone, String name, Date birth,
 			String gender, String nickName, String imgURL, String status, String grade, Date userDate) {
