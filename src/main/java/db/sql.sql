8e944ef2ce82dee427d43c41187c7ecceb98ec51
완주 <wjwan0915@gmail.com>
2021-07-08 20:28:36 +0900

    no message

--- src/main/java/db/sql.sql
+++ src/main/java/db/sql.sql
@@ -5,4 +5,5 @@ SELECT * FROM REVIEW;
 select * from user_sequences;
                   
 SELECT MAX(MovieNum) FROM SP_PLATFORM WHERE PFCODE IN 'WC';
+djpowa;
 SELECT MovieNum FROM SP_CONTENTS SC WHERE SC.MovieNum > 2;
