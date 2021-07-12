06c152091b4b635be1db42181eadcf24a6dc5116
Ju Dong Jin <wnehdwlswn@gmail.com>
2021-07-12 16:54:24 +0900

    wish 2

--- src/main/java/com/controller/WishServlet.java
+++ src/main/java/com/controller/WishServlet.java
@@ -15,15 +15,12 @@ public class WishServlet extends HttpServlet {
     public WishServlet() { super(); }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
