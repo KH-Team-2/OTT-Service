7984c0e81583813e1adf037e5ea7c6da6ebca745
Dahye Jeong <moonbird92@gmail.com>
2021-07-16 20:10:31 +0900

    공지사항 작성/수정

---
+++ src/main/java/com/biz/admin/NoticeWriteBizImpl.java
@@ -0,0 +1,48 @@
+package com.biz.admin;
+
+import static common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import com.dao.admin.NoticeWriteDao;
+import com.dao.admin.NoticeWriteDaoImpl;
+import com.dto.NoticeDto;
+
+public class NoticeWriteBizImpl implements NoticeWriteBiz {
+	private NoticeWriteDao dao = new NoticeWriteDaoImpl();
+	
+	@Override
+	public boolean NoticeWrite(NoticeDto dto) {
+		
+		Connection con = getConnection();
+		
+		boolean res = dao.NoticeWrite(dto, con);
+		
+		if(res) {
+			commit(con);
+		} else {
+			rollback(con);
+		}
+		close(con);
+		
+		return res;
+	}
+
+	@Override
+	public boolean NoticeUpdate(NoticeDto dto) {
+		Connection con = getConnection();
+		
+		boolean res = dao.NoticeUpdate(dto, con);
+		
+		if(res) {
+			commit(con);
+		} else {
+			rollback(con);
+		}
+		
+		close(con);
+		
+		return false;
+	}
+
+}
