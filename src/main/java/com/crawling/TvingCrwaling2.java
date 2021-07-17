997dfe2ec2ec30d57c57011bdbf1ba39cc94196b
Ju Dong Jin <wnehdwlswn@gmail.com>
2021-07-18 02:06:03 +0900

    Wavve Final

---
+++ src/main/java/com/crawling/TvingCrwaling2.java
@@ -0,0 +1,108 @@
+package com.crawling;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import com.dto.ContentsDto;
+import com.dto.TvingDto;
+
+import common.JDBCTemplate;
+
+public class TvingCrwaling2 extends JDBCTemplate{
+	
+	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver"; //드라이버 ID
+    public static final String WEB_DRIVER_PATH = "C:/Users/LazRuby/Desktop/Files/chromedriver.exe"; //드라이버 경로
+
+    public static void main(String[] args) { TvingCrwaling2 c = new TvingCrwaling2(); c.Crawling();}
+    
+    public void Crawling() {
+    	
+    	try { System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH); }
+        catch (Exception e) { e.printStackTrace(); }
+    	
+    	Connection con = getConnection();
+    	ChromeOptions options = new ChromeOptions();
+    	WebDriver driver = new ChromeDriver(options);
+    	Actions actions = new Actions(driver);
+    	JavascriptExecutor jse = (JavascriptExecutor) driver;
+    	
+    	///////////////////////////////////////////////////////////////////////
+    	
+    	String url = "https://www.tving.com/movie/genre";
+    	
+    	driver.get(url);
+    	
+        wait(1000);
+        
+	    for (int i = 0; i < 5; i++) { jse.executeScript("window.scrollBy(0,5000)", ""); wait(2000); }
+	    
+	    System.out.println(driver.findElement(By.xpath("//*[@id=\"movieList\"]/div[4]/a/div[2]")).getText());
+	  
+	    
+	    /*
+	    
+	    for(int i=1; i<101; i++) {
+	    	
+	    	driver.findElement(By.xpath("//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/button ")).click();
+ 		   
+        	java.sql.Date temp_day=  java.sql.Date .valueOf("2020-02-2");
+        	String imgurl = driver.findElement(By.xpath("//*[@id=\"movie-detail-title\"]/div[5]/div[1]/img")).getAttribute("src");
+        	
+        	ContentsDto dto = new ContentsDto(0, "미제공", "미제공", "미제공", "미제공", 5.0, "#드라마", "미제공", "미제공", temp_day, imgurl, "TV" );
+    		
+        	try {
+        		if( driver.findElement(By.xpath("//*[@id=\"wrap\"]/main/div/div/section/div")).getText().length() >= 1 ) {
+        			driver.navigate().back();
+        			continue;
+        		}
+        	} catch ( Exception e ) {}
+        	
+        	wait(2000);
+        	
+        	//////////////////////////////////////////////////////////////////
+        	     	         	        	
+        	dto.setTitle( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/h2/span")  );
+        	dto.setMovieImg(imgurl);
+        	dto.setMovieAddr(driver.getCurrentUrl());
+        	dto.setRate( rannum() );
+        	dto = setDto( driver, dto );
+        	
+        	WavveCrawling selfclass = new WavveCrawling();
+        	selfclass.insert(con, dto);
+        	System.out.println(dto);
+        	
+        	wait(1500);
+        	
+        	driver.navigate().back();
+        	
+        	wait(4000);
+        } 	*/
+      
+	    ///////////////////////////////////////////////////////////////////////
+        
+    }
+    
+    static public void wait(int count) { try { Thread.sleep(count); } catch (InterruptedException e) { } } 
+    
+    static public String getText(WebDriver driver, String url) {  	
+    	String text = "";  	
+    	try { text = driver.findElement(By.xpath(url)).getText(); } catch ( Exception e ) { }	
+    	return text;
+    }
+   
+    static public double rannum() {
+    	double num = (Math.random() * 10);
+        num = Math.round(num*100);
+        num = num / 100.0;
+        return num;
+    }
+}
--- src/main/java/com/crawling/WavveCrawling.java
+++ src/main/java/com/crawling/WavveCrawling.java
@@ -3,6 +3,8 @@ package com.crawling;
 import com.dto.ContentsDto;
 import com.dto.WatchaDto;
 import common.JDBCTemplate;
+
+import org.apache.commons.exec.util.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -65,8 +67,8 @@ public class WavveCrawling extends JDBCTemplate {
         for(int j=1; j<11; j++) {
         	
         	//url = "https://www.wavve.com/list/VN4?api=apis.wavve.com%252Fcf%252Fvod%252Fpopularcontents%253Forderby%253Dviewtime%2526contenttype%253Dvod%2526genre%253D01%2526WeekDay%253Dall%2526uitype%253DVN4%2526uiparent%253DGN56-VN4%2526uirank%253D2%2526broadcastid%253D126147%2526offset%253D0%2526limit%253D20%2526uicode%253DVN4&came=BandViewGnbCode&page=" + j;
-            //url = "https://www.wavve.com/list/VN3?api=apis.wavve.com%252Fcf%252Fvod%252Fpopularcontents%253Forderby%253Dviewtime%2526contenttype%253Dvod%2526genre%253D02%2526WeekDay%253Dall%2526uitype%253DVN3%2526uiparent%253DGN57-VN3%2526uirank%253D2%2526broadcastid%253D164058%2526offset%253D0%2526limit%253D20%2526uicode%253DVN3&came=BandViewGnbCode&page=" + j;
-        	url = "https://www.wavve.com/list/MN85?api=apis.wavve.com%252Fcf%252Fmovie%252Fcontents%253Fsptheme%253Dsvod%2526price%253Dall%2526orderby%253Dviewtime%2526contenttype%253Dmovie%2526genre%253Dall%2526WeekDay%253Dall%2526uitype%253DMN85%2526uiparent%253DGN59-MN85%2526uirank%253D4%2526broadcastid%253D176159%2526offset%253D0%2526limit%253D20%2526uicode%253DMN85%2526mtype%253Dsvod&came=BandViewGnbCode&page=" + j;
+        	url = "https://www.wavve.com/list/VN3?api=apis.wavve.com%252Fcf%252Fvod%252Fpopularcontents%253Forderby%253Dviewtime%2526contenttype%253Dvod%2526genre%253D02%2526WeekDay%253Dall%2526uitype%253DVN3%2526uiparent%253DGN57-VN3%2526uirank%253D2%2526broadcastid%253D164058%2526offset%253D0%2526limit%253D20%2526uicode%253DVN3&came=BandViewGnbCode&page="+j;
+        	//url = "https://www.wavve.com/list/MN85?api=apis.wavve.com%252Fcf%252Fmovie%252Fcontents%253Fsptheme%253Dsvod%2526price%253Dall%2526orderby%253Dviewtime%2526contenttype%253Dmovie%2526genre%253Dall%2526WeekDay%253Dall%2526uitype%253DMN85%2526uiparent%253DGN59-MN85%2526uirank%253D4%2526broadcastid%253D176159%2526offset%253D0%2526limit%253D20%2526uicode%253DMN85%2526mtype%253Dsvod&came=BandViewGnbCode&page=" + j;
             driver.get(url);
             
             wait(1500);  System.out.println();
@@ -77,9 +79,12 @@ public class WavveCrawling extends JDBCTemplate {
             	ContentsDto dto = new ContentsDto(0, "미제공", "미제공", "미제공", "미제공", 5.0, "#드라마", "미제공", "미제공", temp_day, "미제공", "WV" );
         		
             	String imgurl = driver.findElement(By.xpath("//*[@id=\"g-contents\"]/div[2]/div[" + i + "]/a/div[1]/img")).getAttribute("src");
-            	driver.findElement(By.xpath("//*[@id=\"g-contents\"]/div[2]/div[" + i + "]/a/div[2]")).click();
+
+            	wait(300);
             	
-            	wait(1500);
+            	driver.findElement(By.xpath("//*[@id=\"g-contents\"]/div[2]/div[" + i + "]/a/div[2]")).click();
+        	
+            	wait(2000);
             	
             	//////////////////////////////////////////////////////////////////
             	
@@ -89,13 +94,13 @@ public class WavveCrawling extends JDBCTemplate {
             		}
             	} catch ( Exception e ) {}
             	
-            	wait(1500);
+            	wait(2000);
             	
             	//////////////////////////////////////////////////////////////////
             	
             	driver.findElement(By.xpath("//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/button ")).click();
             	
-            	wait(500);
+            	wait(1000);
             	         	        	
             	dto.setTitle( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/h2/span")  );
             	dto.setMovieImg(imgurl);
@@ -105,12 +110,13 @@ public class WavveCrawling extends JDBCTemplate {
             	
             	WavveCrawling selfclass = new WavveCrawling();
             	selfclass.insert(con, dto);
+            	System.out.println(dto);
             	
-            	wait(1000);
+            	wait(1500);
             	
             	driver.navigate().back();
             	
-            	wait(1500); countt++; System.out.println(countt);
+            	wait(4000); countt++; System.out.println(countt);
             } 	
         }
       
@@ -148,28 +154,29 @@ public class WavveCrawling extends JDBCTemplate {
 		return rs>=1?true:false;
     }
     
-    static public void wait(int count) { try { Thread.sleep(count); } catch (InterruptedException e) {} }
+    static public void wait(int count) { try { Thread.sleep(count); } catch (InterruptedException e) { } }
     
     static public String getText(WebDriver driver, String url) { 
     	
     	String text = "";  	
-    	try { text = driver.findElement(By.xpath(url)).getText(); } catch ( Exception e ) {}	
+    	try { text = driver.findElement(By.xpath(url)).getText(); } catch ( Exception e ) { }	
     	return text;
     }
     
     static public ContentsDto setDto(WebDriver driver, ContentsDto dto) {
     	
     	for(int i=1; i<=4; i++) {
-    		
-    		try { if( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]").length() < 1) { continue; } } catch ( Exception e ) {}
-        		
+	
+    		try { if( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]").length() < 1) { continue; } }
+    		catch ( Exception e ) { System.out.println("없는 항목 체크"); continue; }
+      		
     		if ( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]").contains("장르") ) {
     			String text = getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]");
     			text = text.replaceAll("장르", "");
     			text = text.replaceAll(", ,", "");
-    			dto.setGenre("#드라마, " + text);
+    			dto.setGenre("#예능, " + text);
     			continue;
-    		}
+    		} 
     		else if ( getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]").contains("출연") ) {
     			String text = getText(driver, "//*[@id=\"g-contents\"]/div[1]/div/div[2]/div[3]/div/p[" + i + "]");
     			text = text.replaceAll("출연", "");
@@ -192,6 +199,7 @@ public class WavveCrawling extends JDBCTemplate {
     			text = text.replaceAll("[0-9]", "");
     			text = text.replaceAll("개요", "");
     			text = text.replaceAll(", ,", "");
+    			text = lastremove(text);
     			dto.setDirector(text);
     			
     			text2 = text2.replaceAll("1TV|2TV", "");
@@ -219,5 +227,15 @@ public class WavveCrawling extends JDBCTemplate {
         num = Math.round(num*100);
         num = num / 100.0;
         return num;
+    }
+    
+    static public String lastremove(String text) {
+        if (text.length() > 0 && text.charAt(text.length()-1)==' ') {
+          text = text.substring(0, text.length()-1);
+        }
+        if (text.length() > 0 && text.charAt(text.length()-1)==',') {
+            text = text.substring(0, text.length()-1);
+        }
+        return text;
     }
 }
